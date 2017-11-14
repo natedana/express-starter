fdda69576c0cc75963b0fc2ca6654fdd0eee0375
natedana <natepdana@gmail.com>
2017-11-14 10:29:31 -0800

    prune

--- routes/routes.js
+++ routes/routes.js
@@ -3,32 +3,11 @@ var router = express.Router();
 var models = require('../models');
 var User = models.User;
 
-//////////////////////////////// PUBLIC ROUTES ////////////////////////////////
-// Users who are not logged in can see these routes
-
 router.get('/', function(req, res, next) {
   res.render('home');
 });
 
-///////////////////////////// END OF PUBLIC ROUTES /////////////////////////////
+router.get('/listings', (req,res) => {
 
-router.use(function(req, res, next){
-  if (!req.user) {
-    res.redirect('/login');
-  } else {
-    return next();
-  }
-});
-
-//////////////////////////////// PRIVATE ROUTES ////////////////////////////////
-// Only logged in users can see these routes
-
-router.get('/protected', function(req, res, next) {
-  res.render('protectedRoute', {
-    username: req.user.username,
-  });
-});
-
-///////////////////////////// END OF PRIVATE ROUTES /////////////////////////////
-
+})
 module.exports = router;
